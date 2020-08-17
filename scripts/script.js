0990a8f89d274660969bf920ce7487a758f3d9ef
Ahasanul Hoque <ahoque24@gmail.com>
2020-08-17 19:41:59 -0400

    Began work on game flow - game changes turns and renders X or O correctly. Still need to add victory condition

--- scripts/script.js
+++ scripts/script.js
@@ -1,19 +1,78 @@
-const gameBoard = ((doc) => {
-    let board = [doc.querySelector('#square-1').textContent,
-    doc.querySelector('#square-2').textContent,
-    doc.querySelector('#square-3').textContent,
-    doc.querySelector('#square-4').textContent,
-    doc.querySelector('#square-5').textContent,
-    doc.querySelector('#square-6').textContent,
-    doc.querySelector('#square-7').textContent,
-    doc.querySelector('#square-8').textContent,
-    doc.querySelector('#square-9').textContent]
+const board = document.querySelector('#tic-tac-toe-board');
+
+const Gameboard = (() => {
+    let board = ["\u00A0", "\u00A0", "\u00A0",
+                 "\u00A0", "\u00A0", "\u00A0",
+                 "\u00A0",  "\u00A0", "\u00A0"];
 
     return {
-        board: board
+        board
     }
-})(document);
+})();
 
-const Player = (letter) => {
+const DisplayController = (() => {
+    //Function to render array onto page
+    function updateBoard() {
+        for(i=0;i<9;i++){
+            board.querySelector(`#square-${i}`).textContent = Gameboard.board[i];
+        }
+    }
+    updateBoard();
 
+    return {
+        updateBoard
+    }
+})();
+
+//Player object
+const Player = (name, letter) => {
+    //Function to select a square. It will use the selected square's ID
+    //as the index for the board array, and change it to the player's letter
+    function selectSquare(i){
+        Gameboard.board[i] = letter;
+    }
+
+    return {
+        name,
+        letter,
+        selectSquare
+    }
 }
+
+const Gameplay = (() => {
+    let player1 = Player('Bob', 'x');
+    let player2 = Player('Chris', 'o');
+
+    let i=0;
+    function playGame() {
+        //playerTurn is used to determine whose turn it is.
+        //True: player1
+        //False: player2
+        let playerTurn = true;
+
+        //Listen for clicks on board
+        do{
+            board.onclick = function(e) {
+                //Get the square number from last character of target's ID
+                //Update array at square number with player's letter
+                //Update the board
+                //Change the turn
+                let square = e.target;
+                if(playerTurn && square.textContent == "\u00A0"){
+                    player1.selectSquare(square.id.charAt(square.id.length-1));
+                    playerTurn = !playerTurn;
+                }
+                else if(!playerTurn && square.textContent == "\u00A0") {
+                    player2.selectSquare(square.id.charAt(square.id.length-1));
+                    playerTurn = !playerTurn;
+                }
+                DisplayController.updateBoard();
+                
+            }
+            i++;
+        }while(i<9) //Placeholder condition for while loop, will need to
+                    //change it to game's victory conditions
+    }
+    
+    playGame();
+})();
