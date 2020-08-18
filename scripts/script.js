4217608aa5b854eb7846fb277d7dd5a025001075
Ahasanul Hoque <ahoque24@gmail.com>
2020-08-18 12:16:03 -0400

    Updated for victory conditions involving top left square and to allow players to play agin. Still need other victory conditions, draws, and to allow players to enter name

--- scripts/script.js
+++ scripts/script.js
@@ -40,53 +40,85 @@ const Player = (name, letter) => {
 }
 
 const Gameplay = (() => {
-    let player1 = Player('Bob', 'x');
-    let player2 = Player('Chris', 'o');
+    let player1 = Player('X', 'x');
+    let player2 = Player('O', 'o');
+    
+    const winner = document.createElement('div');
+    const replay = document.createElement('button');
+    replay.textContent = 'Play again?';
 
-    let i=0;
-    function playGame() {
-        //playerTurn is used to determine whose turn it is.
-        //True: player1
-        //False: player2
-        let playerTurn = true;
+    //playerTurn is used to determine whose turn it is.
+    //True: player1
+    //False: player2
+    let playerTurn = true;
 
-
-        //Listen for clicks on board
-        do{
-            board.onclick = function(e) {
-                //Get the square number from last character of target's ID
-                //Update array at square number with player's letter
-                //Update the board
-                //Change the turn
-                let square = e.target;
-                if(playerTurn && square.textContent == "\u00A0"){
-                    player1.selectSquare(square.id.charAt(square.id.length-1));
-                    playerTurn = !playerTurn;
-                }
-                else if(!playerTurn && square.textContent == "\u00A0") {
-                    player2.selectSquare(square.id.charAt(square.id.length-1));
-                    playerTurn = !playerTurn;
-                }
-                DisplayController.renderBoard();   
+    const _playGame = () => {
+        //Listen for clicks on board   
+        board.onclick = function(e) {
+            //Get the square number from last character of target's ID
+            //Update array at square number with player's letter
+            //Update the board
+            //Change the turn
+            let square = e.target;
+            if(playerTurn && square.textContent == "\u00A0"){
+                player1.selectSquare(square.id.charAt(square.id.length-1));
             }
-            i++;
-        }while(i<9) //Placeholder condition for while loop, will need to
-                    //change it to game's victory conditions
+            else if(!playerTurn && square.textContent == "\u00A0") {
+                player2.selectSquare(square.id.charAt(square.id.length-1));
+            }
+            //Update the board
+            DisplayController.renderBoard();
+            //Test if game should end, otherwise continue
+            if(_testWin() || Gameboard.board.indexOf("\u00A0") == -1) _endGame(); //Placeholder for ending game
+            else playerTurn = !playerTurn;
+        }
     }
     
-    function testWin() {
+    const _testWin = () => {
         //Funciton to test win conditions
         //Test for array elements to be equal to each other AND not equal to space
-        //Return boolean to be used in playGame() function
+        //Return letter name to determine winner in playGame() function
 
-        //Top row tests
-        //(Gameboard.board[i] == Gameboard.board[j]) && (Gameboard.board[i] == Gameboard.[k]);
-        //return true;
+        //Tests invoving top left square
+        if(Gameboard.board[0] != "\u00A0"){
+            if((Gameboard.board[0] == Gameboard.board[1]) && 
+                (Gameboard.board[1] == Gameboard.board[2])) return true;
+            else if((Gameboard.board[0] == Gameboard.board[3]) &&
+                    (Gameboard.board[3] == Gameboard.board[6])) return true;
+            else if((Gameboard.board[0] == Gameboard.board[4]) &&
+                    (Gameboard.board[4] == Gameboard.board[8])) return true;
+        }
     }
 
-    playGame();
+    const _endGame = () => {
+        //Stop allowing clicks on the board
+        board.onclick = null;
 
-    return {
-        playerTurn
+        //Test for winner and show winner name on page
+        if(playerTurn) winner.textContent = `${player1.name} wins!`;
+        else winner.textContent = `${player2.name} wins!`;
+        board.appendChild(winner);
+
+        //Place the replay button on the page, run _playAgain() if it is clicked
+        board.appendChild(replay);
+        replay.onclick = () => {
+            _playAgain();
+        }
     }
+
+    const _playAgain = () => {
+        //Remove the play again button, set the tic-tac-toe board bac to
+        //non-breaking spaces, and run the _playGame() function again to
+        //play again
+        board.removeChild(winner);
+        board.removeChild(replay);
+        Gameboard.board = ["\u00A0", "\u00A0", "\u00A0",
+                            "\u00A0", "\u00A0", "\u00A0",
+                            "\u00A0",  "\u00A0", "\u00A0"];
+        playerTurn = false;
+        _playGame();
+    }
+
+    //Run playGame() on page load
+    _playGame();
 })();
